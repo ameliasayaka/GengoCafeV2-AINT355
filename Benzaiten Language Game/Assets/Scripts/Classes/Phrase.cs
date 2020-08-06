c96c09945394612c2ebba0bc38e004ba9efdb640
ameliasayaka <ameliasayaka@gmail.com>
2020-08-06 15:13:47 +0100

    Phrasebook additions
    added tag to phrases
    phrases unlock according to player progress

--- Benzaiten Language Game/Assets/Scripts/Classes/Phrase.cs
+++ Benzaiten Language Game/Assets/Scripts/Classes/Phrase.cs
@@ -7,14 +7,27 @@ public class Phrase
 {
     [JsonProperty]
     public string english, japanese;
+    [JsonProperty]
+    public int tag;
+
     public bool isUnlocked;
 
     [JsonConstructor]
-    public Phrase(string english, string japanese)
+    public Phrase(string english, string japanese,int tag)
     {
         this.english = english;
         this.japanese = japanese;
-        isUnlocked = false;
+        this.tag = tag;
+
+        //base phrases automatically unlocked
+        if (tag == 0)
+        { 
+            isUnlocked = true;
+        }
+        else
+        {
+            isUnlocked = false;
+        }
     }
 
     
--- Benzaiten Language Game/Assets/Scripts/PhrasebookManager.cs
+++ Benzaiten Language Game/Assets/Scripts/PhrasebookManager.cs
@@ -7,20 +7,52 @@ public class PhrasebookManager : MonoBehaviour
 {
     private DataHolder dataHolder;
     private PhraseBook phrasebook;
+    private Player player;
     [SerializeField]
     private GameObject phrasePrefab;
     void Start()
     {
         dataHolder = GameObject.Find("DataHolder").GetComponent<DataHolder>();
         phrasebook = dataHolder.Phrasebook;
+        player = dataHolder.player;
+        
+        //check player progression andset unlock
+        
 
         foreach (Phrase phrase in phrasebook.PhraseList)
         {
-            GameObject tempPhrase = Instantiate(phrasePrefab, transform);
-            TextMeshProUGUI[] textList = tempPhrase.GetComponentsInChildren<TextMeshProUGUI>();
+            //after intro with guide unlock kaoru associated phrases
+           if (phrase.tag == 1)
+            {
+                if(player.PlayerProgression["Guide"] >= 1)
+                {
+                    phrase.isUnlocked = true;
+                }
+                else
+                {
+                    phrase.isUnlocked = false;
+                }
+            }
+           else if (phrase.tag ==2)
+            {
+                if (player.PlayerProgression["Kaoru"] >= 1)
+                {
+                    phrase.isUnlocked = true;
+                }
+                else
+                {
+                    phrase.isUnlocked = false;
+                }
+            }
+               
+            if (phrase.isUnlocked == true)
+            {
+                GameObject tempPhrase = Instantiate(phrasePrefab, transform);
+                TextMeshProUGUI[] textList = tempPhrase.GetComponentsInChildren<TextMeshProUGUI>();
 
-            textList[0].text = phrase.japanese.Replace("~~", dataHolder.player.PlayerName);
-            textList[1].text = phrase.english.Replace("~~", dataHolder.player.PlayerName);
+                textList[0].text = phrase.japanese.Replace("~~", dataHolder.player.PlayerName);
+                textList[1].text = phrase.english.Replace("~~", dataHolder.player.PlayerName);
+            }
         }
     }
 }
